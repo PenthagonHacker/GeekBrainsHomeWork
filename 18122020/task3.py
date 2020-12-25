1e5b2613a991cb93cf07459fed76598526e1ad6d
geekbrains <vladislavmasalov@yandex.ru>
2020-12-26 00:59:01 +0300

    deleted superfluous folder

--- 18122020/task3.py
+++
@@ -1,5 +0,0 @@
-n = input('Введите число: ')
-
-sumn = int(n) + int(n * 2) + int(n * 3)
-
-print(sumn)
--- 18122020/task4.py
+++
@@ -1,9 +0,0 @@
-number = int(input('Введите число: '))
-max = 0
-while number > 10:
-    figure = number % 10
-    if figure > max:
-        max = figure
-    number //= 10
-
-print(max)
