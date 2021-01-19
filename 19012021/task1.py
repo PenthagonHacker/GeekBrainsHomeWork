0b5ca9722aa2b0b96d0498a85353d49a968cbb9a
geekbrains <vladislavmasalov@yandex.ru>
2021-01-20 00:14:41 +0300

    Добавил версию без применения numpy. Осталось написать проверку на совместимость матриц

--- 19012021/task1.py
+++ 19012021/task1.py
@@ -3,18 +3,32 @@ class Matrix:
         self.lst = lst
 
     def __str__(self):
-        import numpy as np
-        m = np.array(self.lst)
-        return '\n'.join(str(r) for r in m)
+        return '\n'.join(str(r) for r in self.lst)
+
+    def compatibility_check(self):  # проверка на одинаковые размеры матриц
+        pass
+        # todo if len(self.lst) == len(other.lst) and something else:
+        #       checking
+        #        return True
+        #     else:
+        #        return f'Данные матрицы не подлежат сложению'
 
     def __add__(self, other):
-        import numpy as np
-        return Matrix(np.array(self.lst) + np.array(other.lst))
+        result = []
+        for row in range(len(self.lst)):
+            for elem in range(len(self.lst[0])):
+                result.append(self.lst[row][elem] + other.lst[row][elem])  # получил сумму соотв. элементов
+                self.result1 = [result[i: i + len(self.lst[0])] for i in
+                                range(0, len(result), len(self.lst[0]))]  # разделил список на списки
+        return Matrix(self.result1)
+
 
 
 matrix = Matrix([[1, 2, 3], [4, 5, 6]])
 matrix1 = Matrix([[11, 22, 33], [44, 55, 66]])
-
+#
 print(matrix, '\n')
 print(matrix1, '\n')
 print(matrix + matrix1)
+
+
