432fdcd6850aba92419ebf3c86417c2f3a02e516
geekbrains <vladislavmasalov@yandex.ru>
2021-01-23 16:08:03 +0300

    Написана логика для игрока: Что работает: 1. Перезапись карточки с новыми зачёркнутыми числами 2. Проверка на наличие числа в карточке игрока 3. Проверка правильности выбора ответа игроком (Y/N) Что требуется написать: 1. Выход из бесконечного цикла при условии всех зачёркнутых в карточке чисел 2. Не плохо было бы дописать проверку на ввод пользователем Y/N, чтобы игра не завершалась при опечатке.

---
+++ 22012021/task888.py
@@ -0,0 +1,41 @@
+class CardGenerator:
+    pass
+
+
+# проверка есть ли число в карточке
+def check_card(barrel):
+    with open('player.txt') as player:
+        file = player.read()
+        if str(barrel) in file:
+            return True
+        else:
+            return False
+
+
+# метод игра#
+with open('player.txt', 'r+', encoding='utf-8') as card:
+    from random import randint
+
+    used_barrels = []
+    while len(used_barrels) < 15:
+        #todo Нам необходимо написать условие выхода из цикла для случая если игрок или комп закрыли все числа в карточке.
+        #todo Я не могу врубиться как оъяснить программе, что все числа в карточке закончились =\
+        card.seek(0)  # 'каретку' в начало файла  иначе не работает !!!!!
+        file = card.read() # читаем содержимое файла
+        barrel = str(randint(1, 91))
+        if barrel not in used_barrels:
+            print(f'Выпало число {barrel}')
+            print(file, end='\n')
+            used_barrels.append(barrel)
+            answer = input('Вы хотите зачеркнуть число? Y (да)/ N(нет)')
+            if any([answer == 'Y' and check_card(barrel), answer == 'N' and check_card(barrel) == False]): # проверка правильности действий игрока
+                  # здесь тоже 'каретку' в начало файла иначе не работает !!!!
+                if len(barrel) == 1:  # проверка если выпало однозначное число
+                    file = file.replace(' ' + barrel + ' ', ' - ')  # чтобы не заменяло цифры в двухзначных числах, если выпало однозначное число
+                else:
+                    file = file.replace(barrel, '-')  # просто заменяем число, если оно двухзначное
+                card.seek(0)
+                card.write(file)
+            else:
+                print('Вы проиграли')
+                break
